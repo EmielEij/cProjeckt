4ca9237b2b9af726521fbbbc0506f015931394c1
EmielEij <50696030+EmielEij@users.noreply.github.com>
2020-02-20 16:56:46 +0100

    Update Compile_ArduinoMonitor.bat

--- Compile_ArduinoMonitor.bat
+++ Compile_ArduinoMonitor.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "C:/Users/Emiel Eij/Desktop/automated projeckts/ArduinoMonitor"
+cd /d "C:\Users\Emiel Eij\Desktop\automated projeckts\ArduinoMonitor\cProjeckt"
 del *.exe
 cmd /c gcc -o ArduinoMonitorExecute ArduinoMonitor.c
 pause
